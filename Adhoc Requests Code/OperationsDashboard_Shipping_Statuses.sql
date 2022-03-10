39fd019a5cf00b662a160047b6b35404c2758e8f
Sanaa Dakdouk <sdakdouk@mypartssource.com>
2022-03-10 17:09:42 -0500

    Operations dashboard statuses code updated to include all customer levels

--- Adhoc Requests Code/OperationsDashboard_Shipping_Statuses.sql
+++ Adhoc Requests Code/OperationsDashboard_Shipping_Statuses.sql
@@ -9,7 +9,7 @@ From   Dw.Line_Item_Details lid
        left join DW.TRACKING_NUMBERS t On lid.LINE_ITEM_ID = t.LINE_ITEM_ID  and t.SHIPMENT_LEG_ID = 1
 where  ss.last_ordered_timestamp is not null 
        and SS.LAST_ORDERED_TIMESTAMP >=  current_timestamp - 3
-       and c.Company_Id in (119, 60891, 4782) 
+       and c.Lvl1_Company_Id in (119, 60891, 4782) 
        and t.LINE_ITEM_ID is null
 Group By c.LVL1_COMPANY_NAME
 with ur;
@@ -18,14 +18,15 @@ with ur;
 Select 'Overnight/Priority - Backordered w/ out ESD', c.LVL1_COMPANY_NAME, Count(distinct lid.line_item_id) as Status_Count
 From   Dw.Line_Item_Details lid
        join DW.LINE_ITEM_STATUS_SUMMARY SS On SS.LINE_ITEM_ID = lid.LINE_ITEM_ID 
+       join DW.BILLING_TRANSACTIONS b on b.line_item_id = lid.LINE_ITEM_ID and b.TRANSACTION_MULTIPLIER = 1
        join DW.CUSTOMERS c on c.COMPANY_ID = lid.CUSTOMER_ID 
        join DW.SHIPPING_INFO s On lid.LINE_ITEM_ID = s.LINE_ITEM_ID
        join DW.Ship_Methods m On s.SHIPPING_METHOD = m.SHIPPING_METHOD and m.TRANSIT_DAYS in (1,2,3)
 where  ss.last_ordered_timestamp is not null 
        and SS.LAST_ORDERED_TIMESTAMP >=  current_timestamp - 3
        and lid.ESTIMATED_SHIPPING_DATE is null 
-       and lid.ESD_DESCRIPTION = 'Backordered'
-       and c.Company_Id in (119, 60891, 4782)
+       and (lid.ESD_DESCRIPTION = 'Backordered' or b.BACKORDER_IND = 1)
+       and c.Lvl1_Company_Id  in (119, 60891, 4782)
 Group By c.LVL1_COMPANY_NAME
 with ur;
 
@@ -33,15 +34,16 @@ with ur;
 Select 'Overnight/Priority - Backordered w/ ESD', c.LVL1_COMPANY_NAME, Count(distinct lid.line_item_id) as Status_Count
 From   Dw.Line_Item_Details lid
        join DW.LINE_ITEM_STATUS_SUMMARY SS On SS.LINE_ITEM_ID = lid.LINE_ITEM_ID 
+       join DW.BILLING_TRANSACTIONS b on b.line_item_id = lid.LINE_ITEM_ID and b.TRANSACTION_MULTIPLIER = 1
        join DW.CUSTOMERS c on c.COMPANY_ID = lid.CUSTOMER_ID 
        join DW.SHIPPING_INFO s On lid.LINE_ITEM_ID = s.LINE_ITEM_ID
        join DW.Ship_Methods m On s.SHIPPING_METHOD = m.SHIPPING_METHOD and m.TRANSIT_DAYS in (1,2,3)
 where  ss.last_ordered_timestamp is not null 
        and SS.LAST_ORDERED_TIMESTAMP >=  current_timestamp - 3
        and lid.ESTIMATED_SHIPPING_DATE is not null 
-       and lid.ESD_DESCRIPTION = 'Backordered'
+       and (lid.ESD_DESCRIPTION = 'Backordered' or b.BACKORDER_IND = 1)
        and lid.LINE_ITEM_STATUS_ID = 25
-       and c.Company_Id in (119, 60891, 4782)
+       and c.Lvl1_Company_Id in (119, 60891, 4782)
 Group By c.LVL1_COMPANY_NAME
 with ur;
 
@@ -55,7 +57,7 @@ From   Dw.Line_Item_Details lid
 where  ss.last_ordered_timestamp is not null 
        and SS.LAST_ORDERED_TIMESTAMP >=  current_timestamp - 3
        and c.CREDIT_HOLD = 'Y'
-       and c.Company_Id in (119, 60891, 4782)
+       and c.Lvl1_Company_Id in (119, 60891, 4782)
 Group By c.LVL1_COMPANY_NAME
 with ur;
 
@@ -70,7 +72,7 @@ From   Dw.Line_Item_Details lid
 where  ss.last_ordered_timestamp is not null 
        and SS.LAST_ORDERED_TIMESTAMP >=  current_timestamp - 3
        and t.DATE_CONFIRMED is null
-       and c.Company_Id in (119, 60891, 4782)
+       and c.Lvl1_Company_Id in (119, 60891, 4782)
 Group By c.LVL1_COMPANY_NAME
 with ur;
 
@@ -83,13 +85,13 @@ From   Dw.Line_Item_Details lid
        join DW.Ship_Methods m On s.SHIPPING_METHOD = m.SHIPPING_METHOD and m.TRANSIT_DAYS in (1,2,3)
 where  ss.last_ordered_timestamp is not null 
        and SS.LAST_ORDERED_TIMESTAMP >=  current_timestamp - 3
-       and c.Company_Id in (119, 60891, 4782)
+       and c.Lvl1_Company_Id in (119, 60891, 4782)
        and lid.LINE_ITEM_STATUS_ID in (1,2)
 Group By c.LVL1_COMPANY_NAME
 with ur;
 
 --Critical Item - Not Ordered
-Select 'Priority - Sourcing awaiting response', c.LVL1_COMPANY_NAME, Count(distinct lid.line_item_id) as Status_Count
+Select 'Critical Item - Not Ordered', c.LVL1_COMPANY_NAME, Count(distinct lid.line_item_id) as Status_Count
 From   Dw.Line_Item_Details lid
        join DW.LINE_ITEM_STATUS_SUMMARY SS On SS.LINE_ITEM_ID = lid.LINE_ITEM_ID 
        join DW.CUSTOMERS c on c.COMPANY_ID = lid.CUSTOMER_ID 
@@ -97,12 +99,12 @@ where  ss.last_ordered_timestamp is not null
        and SS.LAST_ORDERED_TIMESTAMP >=  current_timestamp - 3
        and lid.LINE_ITEM_STATUS_ID = 7
        and lid.URGENCY_CODE = 'STAT'
-       and c.Company_Id in (119, 60891, 4782)
+       and c.Lvl1_Company_Id in (119, 60891, 4782)
 Group By c.LVL1_COMPANY_NAME
 with ur;
 
 --Critical Item - Awaiting Sourcing
-Select 'Priority - Sourcing awaiting response', c.LVL1_COMPANY_NAME, Count(distinct lid.line_item_id) as Status_Count
+Select 'Critical Item - Awaiting Sourcing', c.LVL1_COMPANY_NAME, Count(distinct lid.line_item_id) as Status_Count
 From   Dw.Line_Item_Details lid
        join DW.LINE_ITEM_STATUS_SUMMARY SS On SS.LINE_ITEM_ID = lid.LINE_ITEM_ID 
        join DW.CUSTOMERS c on c.COMPANY_ID = lid.CUSTOMER_ID 
@@ -110,7 +112,7 @@ where  ss.last_ordered_timestamp is not null
        and SS.LAST_ORDERED_TIMESTAMP >=  current_timestamp - 3
        and lid.LINE_ITEM_STATUS_ID in (1,2)
        and lid.URGENCY_CODE = 'STAT'
-       and c.Company_Id in (119, 60891, 4782)
+       and c.Lvl1_Company_Id in (119, 60891, 4782)
 Group By c.LVL1_COMPANY_NAME
 with ur;
 
@@ -124,7 +126,7 @@ where  ss.last_ordered_timestamp is not null
        and lid.LINE_ITEM_STATUS_ID = 3
        and lid.MANUAL_RESEARCH = 1
        and lid.URGENCY_CODE = 'STAT'
-       and c.Company_Id in (119, 60891, 4782)
+       and c.Lvl1_Company_Id in (119, 60891, 4782)
 Group By c.LVL1_COMPANY_NAME
 with ur;
 
@@ -133,13 +135,14 @@ Select 'Change in Estimated Ship Date', c.LVL1_COMPANY_NAME, Count(distinct lid.
 From   Dw.Line_Item_Details lid
        join DW.LINE_ITEM_STATUS_SUMMARY SS On SS.LINE_ITEM_ID = lid.LINE_ITEM_ID 
        join DW.CUSTOMERS c on c.COMPANY_ID = lid.CUSTOMER_ID 
-       join (select count(updated_timestamp) as updated_ship_count, LINE_ITEM_ID 
-             FROM DW.ESTIMATED_SHIP_INFO 
-             where ESD <> updated_timestamp
+       join (select count(updated_timestamp) as updated_ship_count, 
+                    LINE_ITEM_ID 
+             FROM   DW.ESTIMATED_SHIP_INFO 
+             where  ESD <> updated_timestamp
              group by line_item_id) ESD on ESD.line_Item_Id = lid.LINE_ITEM_ID and updated_ship_count > 1
 where  ss.last_ordered_timestamp is not null 
        and SS.LAST_ORDERED_TIMESTAMP >= current_timestamp - 3
-       and c.Company_Id in (119, 60891, 4782)
+       and c.Lvl1_Company_Id in (119, 60891, 4782)
 Group By c.LVL1_COMPANY_NAME
 with ur;
 
@@ -147,11 +150,12 @@ with ur;
 Select 'Order didn’t ship on schedule / past ESD', c.LVL1_COMPANY_NAME, Count(distinct lid.line_item_id) as Status_Count
 From   Dw.Line_Item_Details lid
        join DW.LINE_ITEM_STATUS_SUMMARY SS On SS.LINE_ITEM_ID = lid.LINE_ITEM_ID 
+       join DW.TRACKING_NUMBERS t On lid.LINE_ITEM_ID = t.LINE_ITEM_ID  and t.SHIPMENT_LEG_ID = 1
        join DW.CUSTOMERS c on c.COMPANY_ID = lid.CUSTOMER_ID 
 where  ss.last_ordered_timestamp is not null 
        and SS.LAST_ORDERED_TIMESTAMP >= current_timestamp - 3
-       and (current_timestamp - 1 > lid.ESTIMATED_SHIPPING_DATE or current_timestamp - 2 > lid.ESTIMATED_SHIPPING_DATE or current_timestamp - 3 > lid.ESTIMATED_SHIPPING_DATE)
-       and c.Company_Id in (119, 60891, 4782)
+       and t.DATE_SHIPPED > lid.ESTIMATED_SHIPPING_DATE 
+       and c.Lvl1_Company_Id in (119, 60891, 4782)
 Group By c.LVL1_COMPANY_NAME
 with ur;
 
@@ -167,7 +171,7 @@ where  ss.last_ordered_timestamp is not null
        and lid.LINE_ITEM_STATUS_ID = 25
        and lid.SMART_ORDER_CONF = 0
        and Time(coalesce(v.VEND_CUTOFF_EST, '4:00:00')) - Time(Current_timestamp) < 23000
-       and c.Company_Id in (119, 60891, 4782)
+       and c.Lvl1_Company_Id in (119, 60891, 4782)
 Group By c.LVL1_COMPANY_NAME
 with ur;
 
@@ -182,7 +186,7 @@ where  ss.last_ordered_timestamp is not null
        and lid.LINE_ITEM_STATUS_ID = 25
        and lid.SMART_ORDER_CONF = 0
        and Time(Current_timestamp) > Time(coalesce(v.VEND_CUTOFF_EST, '4:00:00'))
-       and c.Company_Id in (119, 60891, 4782)
+       and c.Lvl1_Company_Id in (119, 60891, 4782)
 Group By c.LVL1_COMPANY_NAME
 with ur;
 
@@ -190,27 +194,30 @@ with ur;
 Select 'Item - Backordered w/ out ESD', c.LVL1_COMPANY_NAME, Count(distinct lid.line_item_id) as Status_Count
 From   Dw.Line_Item_Details lid
        join DW.LINE_ITEM_STATUS_SUMMARY SS On SS.LINE_ITEM_ID = lid.LINE_ITEM_ID 
+       join DW.BILLING_TRANSACTIONS b on b.LINE_ITEM_ID = lid.LINE_ITEM_ID and b.TRANSACTION_MULTIPLIER = 1
        join DW.CUSTOMERS c on c.COMPANY_ID = lid.CUSTOMER_ID 
 where  ss.last_ordered_timestamp is not null 
        and SS.LAST_ORDERED_TIMESTAMP >=  current_timestamp - 3
        and lid.ESTIMATED_SHIPPING_DATE is null 
-       and lid.ESD_DESCRIPTION = 'Backordered'
+       and (lid.ESD_DESCRIPTION = 'Backordered' or b.BACKORDER_IND = 1)
        and lid.LINE_ITEM_STATUS_ID = 25
-       and c.Company_Id in (119, 60891, 4782)
+       and c.Lvl1_Company_Id in (119, 60891, 4782)
 Group By c.LVL1_COMPANY_NAME
 with ur;
 
 
 --Item - Backordered w/ ESD
-Select 'Item - Backordered w/ out ESD', c.LVL1_COMPANY_NAME, Count(distinct lid.line_item_id) as Status_Count
+Select 'Item - Backordered w/ ESD', c.LVL1_COMPANY_NAME, Count(distinct lid.line_item_id) as Status_Count
 From   Dw.Line_Item_Details lid
        join DW.LINE_ITEM_STATUS_SUMMARY SS On SS.LINE_ITEM_ID = lid.LINE_ITEM_ID 
+       left join DW.BILLING_TRANSACTIONS b on b.LINE_ITEM_ID = lid.LINE_ITEM_ID and b.TRANSACTION_MULTIPLIER = 1
        join DW.CUSTOMERS c on c.COMPANY_ID = lid.CUSTOMER_ID 
 where  ss.last_ordered_timestamp is not null 
        and SS.LAST_ORDERED_TIMESTAMP >=  current_timestamp - 3
        and lid.ESTIMATED_SHIPPING_DATE is not null 
-       and lid.ESD_DESCRIPTION = 'Backordered'
+       and (lid.ESD_DESCRIPTION = 'Backordered' or b.BACKORDER_IND = 1)
        and lid.LINE_ITEM_STATUS_ID = 25
-       and c.Company_Id in (119, 60891, 4782)
+       and c.Lvl1_Company_Id in (119, 60891, 4782)
 Group By c.LVL1_COMPANY_NAME
 with ur;
+
